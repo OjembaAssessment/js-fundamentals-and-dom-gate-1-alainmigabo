da9d39b2ca5b3f26031d3c494545e4532c769fdc
alainmigabo <alainmigabo2k03@gmail.com>
2023-07-24 09:44:23 +0200

    deployment check

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,1 +1,15 @@
 console.log("Exercise 1 - Word Highlighter");
+
+const paragraph = document.getElementById('myParagraph').innerText
+
+console.log(paragraph.split(' '))
+
+const myObj = {
+    the: 1,
+    european: 1,
+    languages: 1,
+    are: 1,
+    members: 1
+};
+
+console.log(myObj.european++)

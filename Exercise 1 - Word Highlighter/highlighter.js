c569bda3f05e05e24853ce8abcef222774b82053
alainmigabo <alainmigabo2k03@gmail.com>
2023-07-24 11:30:53 +0200

    unfinished question 1

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,15 +1,100 @@
 console.log("Exercise 1 - Word Highlighter");
 
 const paragraph = document.getElementById('myParagraph').innerText
+const paragraphObj = {};
 
-console.log(paragraph.split(' '))
+paragraph.split(' ').map(elt=>{
+    let newarr = []
+    if(elt.includes('.\n')){
+        newarr = elt.split('.\n');
+        return (newarr[0],newarr[1])
+    }
+    else if(elt.includes(`"`)){
+        newarr = elt.split(`"`);
+        return (newarr[1] !== "") ? (newarr[1]) : (newarr[2])
+    }
+    else if(elt.includes(",")){
+        newarr = elt.split(",");
+        return (newarr[1] !== "") ? (newarr[1]) : (newarr[2])
+    }
+    else if(elt.includes(".")){
+        newarr = elt.split(".");
+        return (newarr[1] !== "") ? (newarr[1]) : (newarr[2])
+    }
+    else if(elt.includes(";")){
+        newarr = elt.split(";");
+        return (newarr[1] !== "") ? (newarr[1]) : (newarr[2])
+    }
+    else if(elt.includes(":")){
+        newarr = elt.split(":");
+        return (newarr[1] !== "") ? (newarr[1]) : (newarr[2])
+    }
+    else{
+        return elt;
+    }
+}).forEach(elt=>{
+    if(!Object.keys(paragraphObj).includes(elt)){
+        paragraphObj[elt] = 1;
+    }
+    else{
+        paragraphObj[elt]++;
+    }
+})
 
-const myObj = {
-    the: 1,
-    european: 1,
-    languages: 1,
-    are: 1,
-    members: 1
-};
+let maxes = [Math.max(...Object.values(paragraphObj))]
 
-console.log(myObj.european++)
+Object.values(paragraphObj).sort((a,b)=>b-a).forEach(elt=>{
+    if(!maxes.includes(elt) && (elt < Math.min(...maxes)) && maxes.length<5){
+        maxes.push(elt)
+    }
+})
+
+let toBeHighlighted = Object.keys(paragraphObj).map(elt=>{
+    if(maxes.includes(paragraphObj[elt])){
+        return elt
+    }
+})
+
+while(toBeHighlighted.includes(undefined)){
+    toBeHighlighted.splice(toBeHighlighted.indexOf(undefined),1)
+}
+
+console.log(toBeHighlighted,'to be highlighted')
+
+document.getElementById('myParagraph').innerHTML = paragraph.split(' ').map(elt=>{
+    if(toBeHighlighted.includes(elt)){
+        return "<span style='background-color: yellow;'>"+elt+'</span>'
+    }
+    else{
+        return elt
+    }
+    // if(elt.includes('.\n')){
+    //     newarr = elt.split('.\n');
+    //     return (newarr[0],newarr[1])
+    // }
+    // else if(elt.includes(`"`)){
+    //     newarr = elt.split(`"`);
+    //     return (newarr[1] !== "") ? (newarr[1]) : (newarr[2])
+    // }
+    // else if(elt.includes(",")){
+    //     newarr = elt.split(",");
+    //     return (newarr[1] !== "") ? (newarr[1]) : (newarr[2])
+    // }
+    // else if(elt.includes(".")){
+    //     newarr = elt.split(".");
+    //     return (newarr[1] !== "") ? (newarr[1]) : (newarr[2])
+    // }
+    // else if(elt.includes(";")){
+    //     newarr = elt.split(";");
+    //     return (newarr[1] !== "") ? (newarr[1]) : (newarr[2])
+    // }
+    // else if(elt.includes(":")){
+    //     newarr = elt.split(":");
+    //     return (newarr[1] !== "") ? (newarr[1]) : (newarr[2])
+    // }
+    // else{
+    //     return elt;
+    // }
+}).join(' ')
+
+console.log(maxes,'maxs')
